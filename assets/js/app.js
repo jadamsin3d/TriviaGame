bea59a48ab2e2f5a2873e754d6ca3dd1460e55fa
jadamsin3d <jadamsin3d@gmail.com>
2018-11-03 19:43:55 -0700

    couldn't figure out the reset button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,51 @@
 //create an array containing quiz questions
 //array contains objects with the correct answers and incorrect answers
 
-let Ques = [{ Q1: [{ question: "Green is the best color?", answer: [{ correct: "yes", incorrect1: "no", incorrect2: "sometimes", incorrect3: "never" }] }] }, {
-    Q2: [{ question: "Yellow is part of the alphabet?", answer: [{ correct: "no", incorrect1: "yes", incorrect2: "sometimes", incorrect3: "never" }] }]
-}, {
-    Q3: [{ question: "My bank account money is still there?", answer: [{ correct: "no", incorrect1: "yes", incorrect2: "sometimes", incorrect3: "never" }] }]
-}, {
-    Q4: [{ question: "Made you look?", answer: [{ correct: "yes", incorrect1: "no", incorrect2: "sometimes", incorrect3: "you almost got me" }] }]
-}];
+let Ques = [{
+    question: "Green is the best color?",
+    choice: {
+        a: "A. yes",
+        b: "B. no",
+        c: "C. sometimes",
+        d: "D. never"
+    },
+    gif: "<img src='assets/art/gif1.gif'>",
+    correct: "A. yes"
+},
+{
+    question: "Yellow is part of the alphabet?",
+    choice: {
+        a: "A. no",
+        b: "B. yes",
+        c: "C. sometimes",
+        d: "D. never"
+    },
+    gif: "<img src= 'assets/art/gif2.gif'>",
+    correct: "C. sometimes"
+},
+{
+    question: "My bank account money is still there?",
+    choice: {
+        a: "A. no",
+        b: "B. yes",
+        c: "C. sometimes",
+        d: "D. never"
+    },
+    gif: "<img src='assets/art/gif3.gif'>",
+    correct: "D. never"
+},
+{
+    question: "Made you look?",
+    choice: {
+        a: "A. yes",
+        b: "B. no",
+        c: "C. sometimes",
+        d: "D. you almost got me"
+    },
+    gif: "<img src='assets/art/gif4.gif'>",
+    correct: "D. you almost got me"
+}
+];
 
 
 //create an array to contain the questions that have already been answered so that they do not appear again in the series of questions
@@ -18,20 +56,121 @@ let correct = 0;
 let incorrect = 0;
 let timeout = 0;
 let gameover = false;
+let timer = 15;
+let interval = setInterval(qTimer, 1000);
+let qNum;
+let resetbtn;
+
+function qTimer() {
+    timer--;
+    $(".timerdiv").html("Time remaining: " + timer);
+    if(timer == 0) {
+        $(".answerbox").html("<p>" + 'You ran out of time!' + "</p>");
+        timeout++;;
+        timer = 4;
+        answeredQues.push(Ques[qNum]);
+        Ques.splice(qNum, 1);
+        if(answeredQues.length == 4) {
+            resetbtn = document.createElement('button');
+            resetbtn.setAttribute("class", "resetButton");
+            resetbtn.innerHTML = "Reset";
+            $(".correctcounter").html(resetbtn);
+            clearInterval(interval);
+            $(".questionbox").text("You got " + correct + " correct. Good Job!");
+            $(".answerbox").text("You also got " + incorrect + " incorrect. Still... Good Job!");
+
+            $(".incorrectcounter").text("");
+            $(".timeoutcounter").text("");
+        }
+        $(".timeoutcounter").html("<p>" + 'Timeouts: ' + timeout + "</p>")
+        setTimeout(genQuestion, 3000);
+        
+    }
+}
+
+$(resetbtn).on("click", function () {
+    setTimeout(location.reload(true), t);
+});
 
 
 //create function that pulls a random question from the array
 function genQuestion() {
-    let qNum = Math.floor(Math.random() * 4);
-    $(".display-4").text(qNum); 
-    console.log(qNum);
+    timer = 15;
+    qTimer();
+    qNum = Math.floor(Math.random() * Ques.length);
+    $(".questionbox").text(Ques[qNum].question);
+    let btnA = document.createElement('button');
+    let btnB = document.createElement('button');
+    let btnC = document.createElement('button');
+    let btnD = document.createElement('button');
+    btnA.setAttribute("class", "mcButton");
+    btnB.setAttribute("class", "mcButton");
+    btnC.setAttribute("class", "mcButton");
+    btnD.setAttribute("class", "mcButton");
+    btnA.innerHTML = Ques[qNum].choice.a;
+    btnB.innerHTML = Ques[qNum].choice.b;
+    btnC.innerHTML = Ques[qNum].choice.c;
+    btnD.innerHTML = Ques[qNum].choice.d;
+    $(".answerbox").html(btnA);
+    $(".answerbox").append(btnB);
+    $(".answerbox").append(btnC);
+    $(".answerbox").append(btnD);
+    $(".correctcounter").html("<p>" + 'Correct: ' + correct + "</p>");
+    $(".incorrectcounter").html("<p>" + 'Incorrect: ' + incorrect + "</p>");
+    
+    $(".mcButton").on("click", function () {
+        if(this.innerHTML == Ques[qNum].correct) {
+            $(".answerbox").html("<p>" + 'Correct!' + "</p>");
+            $(".questionbox").html(Ques[qNum].gif);
+            correct++;
+            clearInterval(interval);
+            answeredQues.push(Ques[qNum]);
+            Ques.splice(qNum, 1);
+            if(answeredQues.length == 4) {
+                clearInterval(interval);
+                $(".questionbox").text("You got " + correct + " correct. Good Job!");
+                $(".answerbox").text("You also got " + incorrect + " incorrect. Still... Good Job!");
+                $(".correctcounter").text("");
+                $(".incorrectcounter").text("");
+                $(".timeoutcounter").text("");
+            }
+            setTimeout(genQuestion, 3000);
+            }
+        else {
+            $(".answerbox").html("<p>" + 'You were almost correct!' + "</p>");
+            $(".questionbox").html(Ques[qNum].gif);
+            incorrect++;
+            clearInterval(interval);
+            answeredQues.push(Ques[qNum]);
+            Ques.splice(qNum, 1);
+            if(answeredQues.length == 4) {
+                clearInterval(interval);
+                $(".questionbox").text("You got " + correct + " correct. Good Job!");
+                $(".answerbox").text("You also got " + incorrect + " incorrect. Still... Good Job!");
+                $(".correctcounter").text("");
+                $(".incorrectcounter").text("");
+                $(".timeoutcounter").text("");
+            }
+            setTimeout(genQuestion, 3000);
+            
+        }
+    });
+
 };
 
-genQuestion(Ques);
+if(answeredQues.length == 4) {
+    clearInterval(interval);
+    $(".questionbox").text("You got " + correct + " correct. Good Job!");
+    $(".answerbox").text("You also got " + incorrect + "incorrect. Still... Good Job!");
+    $(".correctcounter").text("");
+    $(".incorrectcounter").text("");
+    $(".timeoutcounter").text("");
+}
+    genQuestion(Ques);
+
 
 
 //create a reset functions that resets everything to its original state
 
 
 //create a confirmation message that confirms whether the answer was correct or in correct.
-
